220716cb7f4cbf24c161d3cf831d629c368f9d52
JamesType <tnt_james_yu1@yahoo.com>
2025-07-09 23:40:15 +0900

    Ensure <chrono> is included for std::chrono

--- engine/3rdparty/JoltPhysics/Jolt/Core/JobSystemThreadPool.cpp
+++ engine/3rdparty/JoltPhysics/Jolt/Core/JobSystemThreadPool.cpp
@@ -9,6 +9,7 @@
 
 JPH_SUPPRESS_WARNINGS_STD_BEGIN
 #include <algorithm>
+#include <chrono> // Ensure this header is included for std::chrono
 JPH_SUPPRESS_WARNINGS_STD_END
 
 #ifdef JPH_PLATFORM_WINDOWS
